0c603c41336875a128d99b5ad47ea7adb52312bc
Hitarth Pattani <hitarth.pattani1@t-mobile.com>
2025-10-01 20:35:21 -0400

    Create onboarding script

--- scripts/onboard-events/index.js
+++ scripts/onboard-events/index.js
@@ -4,6 +4,10 @@
 
 require('dotenv').config();
 
+const fs = require('fs');
+const yaml = require('js-yaml');
+const path = require("path");
+
 const { AdobeAuth, BearerToken, OnboardEvents } = require("@adobe-commerce/aio-toolkit");
 
 async function index () {
@@ -17,33 +21,33 @@ async function index () {
             process?.env?.OAUTH_SCOPES.split(", ") || []
         );
 
-        if (!token) {
-            throw new Error('Token generation failed - no token returned');
+        if (token) {
+            const tokenInfo = BearerToken.info(token);
+            if (tokenInfo.isValid) {
+                const onboardEvents = new OnboardEvents(
+                    "HPattani Sandbox Connector",
+                    process?.env?.IO_CONSUMER_ID || '',
+                    process?.env?.IO_PROJECT_ID || '',
+                    process?.env?.IO_WORKSPACE_ID || '',
+                    process?.env?.OAUTH_CLIENT_ID || '',
+                    tokenInfo.token
+                );
+                onboardEvents.getLogger().debug('[INFO] Token generated successfully');
+                onboardEvents.getLogger().debug(`[INFO] Token expires at: ${tokenInfo.expiry ? tokenInfo.expiry : 'Unknown'}`);
+
+                const ioEventsFileContents = fs.readFileSync(path.resolve(__dirname, "./io-events-config.yaml"), 'utf8');
+                const ioEventsConfig = yaml.load(ioEventsFileContents, {});
+                const ioEventsResponse = await onboardEvents.process(ioEventsConfig);
+
+                console.log(ioEventsResponse.createdProviders[0].raw)
+            } else {
+                console.log('Token is not valid');
+            }
+        } else {
+            console.log('Token generation failed - no token returned');
         }
-
-        const tokenInfo = BearerToken.info(token);
-
-        if(!tokenInfo.isValid) {
-            throw new Error('Token is not valid');
-        }
-
-        const onboardEvents = new OnboardEvents(
-            "HPattani AIO Toolkit V1",
-            process?.env?.IO_CONSUMER_ID || '',
-            process?.env?.IO_PROJECT_ID || '',
-            process?.env?.IO_WORKSPACE_ID || '',
-            process?.env?.OAUTH_CLIENT_ID || '',
-            tokenInfo.token
-        );
-
-        onboardEvents.getLogger().debug('[INFO] Token generated successfully');
-        onboardEvents.getLogger().debug(`[INFO] Token expires at: ${tokenInfo.expiry ? tokenInfo.expiry : 'Unknown'}`);
-
-        const ioEventsConfig = require('./config/io-events-config.json');
-        await onboardEvents.process(ioEventsConfig);
     } catch (error) {
         console.error('Error in main function:', error.message);
-        throw error;
     }
 }
 
